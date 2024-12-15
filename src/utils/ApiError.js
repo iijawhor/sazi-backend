49f6158f06a0104f6d8d0dee9968b129e2f218e8
iijawhor <jawhorali1@gmail.com>
2024-12-16 00:38:36 +0530

    Typo fixed : constructore to constructor

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -14,7 +14,7 @@ class ApiError extends Error {
     if (stack) {
       this.stack = stack;
     } else {
-      Error.captureStackTrace(this, this.constructore);
+      Error.captureStackTrace(this, this.constructor);
     }
   }
 }
